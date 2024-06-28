fc0a7f1121dd55fd11ed1d133fc736c29480d413
ainobin <ainobin9@gmail.com>
2024-06-28 21:46:07 +0600

    conest3

---
+++ 1989A.cpp
@@ -0,0 +1,30 @@
+// This Code is written by Nobin
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define YES     cout << "YES" << "\n"
+#define Yes     cout << "Yes" << "\n"
+#define yes     cout << "yes" << "\n"
+#define NO      cout << "NO" << "\n"
+#define No      cout << "No" << "\n"
+#define no      cout << "no" << "\n"
+#define ll      long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;  cin>>t;
+    while (t--)
+    {
+        int x,y;   cin>>x>>y;
+        if(y>=-1){
+            YES;
+
+        }else{
+            NO;
+        }
+    }
+    return 0;
+}
