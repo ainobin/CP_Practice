da4903274e7aa7f31d79290ac6a2a491008985f9
ainobin <ainobin9@gmail.com>
2024-12-31 11:48:50 +0600

    gitignore test01

---
+++ 2039C2.cpp
@@ -0,0 +1,30 @@
+// This Code is written by Nobin
+// Verdict: AC
+// problem linK:
+// problem: 
+// aproch: 
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+#define ll      long long
+const int mod = 1e9+7;
+const int MAX = 1e5+7;
+
+void solve(){
+    cout<<" ";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+       int t = 1;	
+       cin>>t;
+       while(t--){
+       solve();
+    }
+    
+    
+    return 0;
+}
